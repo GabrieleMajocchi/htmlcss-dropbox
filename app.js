e15fa6ea6ec9496ebe05c0f94453440348e14065
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-04-17 17:45:31 +0200

    test dark mode work in progress

---
+++ app.js
@@ -0,0 +1,8 @@
+
+const toggledark = document.getElementById('toggledark');
+
+toggledark.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    document.getElementsByClassName("test").classList.toggle('dark');
+    console.log('ciao');
+})
